2c52f54cb316d6b7ae4021b1eb518900eda59525
Elee Lawleit <alibutt200235@gmail.com>
2024-04-01 09:50:57 +0500

    some angular testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test-project';
+  count: number = 0;
+  show: boolean = true;
+  frameworks: string[] = ["react", "angular", "vue", "nextjs"]
+
+  increment(event: MouseEvent) {
+    this.count = this.count + 1;
+    console.log(`Clicked on X: ${event.clientX}, Y: ${event.clientY}`);
+  }
+  decrement() {
+    this.count = this.count - 1;
+  }
+
+  showHidePara(){
+    console.log(this.show)
+    this.show = !this.show;
+    console.log(this.show)
+  }
+
 }
